cb09fda9b3a5bfc90d9d349286837287a2752067
akilagrepo <akilaag@gmail.com>
2025-09-17 11:42:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Flask! 🚀 CI/CD with Docker works!"
+    return "Hello, Flask! 🚀 CI/CD with Docker working pradeep success!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
