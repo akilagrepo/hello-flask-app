abd9b812d69c6c1a027b69b418e58160698a2446
akilagrepo <akilaag@gmail.com>
2025-09-17 10:47:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello World from Flask + Jenkins + Docker!"
+def hello():
+    return "Hello, Flask! CI/CD Works 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
